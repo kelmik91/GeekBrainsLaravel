826bc43f4caa011a5735d2bc2570720e967b1ea0
Kelmik91 <kelmik91@me.com>
2020-03-16 23:32:44 +0300

    Home work lesson-3

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -26,63 +26,13 @@
                 width: 165px;
                 margin: auto;
             }
-
-            .full-height {
-                height: 50vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
-                <div class="title m-b-md">
-                    Авторизация
-                </div>
-
-                <div class="links">
-                    <a href="/">Home</a>
-                    <a href="/welcomeUser">welcome</a>
-                    <a href="auth">Авторизация</a>
-                    <a href="/news">News</a>
-                </div>
+                @include('layout.header')
+                @include('layout.nav')
 
                 <form action="#" method="post">
                     <label for="login">Login</label>
@@ -91,6 +41,8 @@
                     <input type="password">
                     <input type="submit">
                 </form>
+
+                @include('layout.footer')
             </div>
         </div>
     </body>
--- resources/views/welcomeUser.blade.php
+++ resources/views/welcomeUser.blade.php
@@ -19,78 +19,11 @@
                 height: 100vh;
                 margin: 0;
             }
-
-            .full-height {
-                height: 50vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Welcome User
-                </div>
-
-                <div class="links">
-                    <a href="/">Home</a>
-                    <a href="/welcomeUser">welcome</a>
-                    <a href="auth">Авторизация</a>
-                    <a href="/news">News</a>
-                </div>
-            </div>
-        </div>
+    @include('layout.header')
+    @include('layout.nav')
+    @include('layout.footer')
     </body>
 </html>
