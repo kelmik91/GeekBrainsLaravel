a3b2e6e0142a56fdbe3ac66423054174727e43b0
Kelmik91 <kelmik91@me.com>
2020-04-10 21:54:40 +0300

    Home work lesson-9

---
+++ app/Http/Controllers/SocialController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\UserRepositories;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialController extends Controller
+{
+    public function loginVk() {
+        if (Auth::id()) {
+            return redirect()->route('home');
+        }
+
+        return Socialite::with('vkontakte')->redirect();
+    }
+
+    public function responseVk(UserRepositories $userRepository) {
+        if (Auth::id()) {
+            return redirect()->route('home');
+        }
+
+        $user = Socialite::driver('vkontakte')->user();
+        session(['soc.token' => $user->token]);
+        $userInSystem = $userRepository->getUserBySocId($user, 'vk');
+        Auth::login($userInSystem);
+
+        return redirect()->route('home');
+    }
+
+    public function loginFb() {
+        if (Auth::id()) {
+            return redirect()->route('home');
+        }
+
+        return Socialite::with('facebook')->redirect();
+    }
+
+    public function responseFb(UserRepositories $userRepository) {
+        if (Auth::id()) {
+            return redirect()->route('home');
+        }
+        
+        $user = Socialite::driver('facebook')->user();
+        session(['soc.token' => $user->token]);
+        $userInSystem = $userRepository->getUserBySocId($user, 'fb');
+        Auth::login($userInSystem);
+
+        return redirect()->route('home');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','role'
+        'name', 'email', 'password','role', 'id_in_soc', 'type_auth', 'avatar'
     ];
 
     public static function rules() {
